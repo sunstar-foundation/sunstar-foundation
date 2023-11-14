e79c83aef77e5ae9c101a176c545cee193383ecd
Dragoș-Cristian Bute <44490413+dnbute@users.noreply.github.com>
2023-11-14 10:19:46 +0200

    Added about-us split (#11)
    * added about-us split
    
    * removed centre content block
    
    * fixed typo
    
    ---------
    
    Co-authored-by: Dragos Cristian Bute <dbute@dragoss-mbp.eur.adobe.com>

--- blocks/center-content/center-content.js
+++
@@ -1,9 +0,0 @@
-export default function decorate(block) {
-  const buttonContainers = block.querySelectorAll('.button-container');
-  const buttonWrapper = document.createElement('div');
-  buttonWrapper.classList.add('button-wrapper');
-  buttonContainers.forEach((buttonContainer) => {
-    buttonWrapper.appendChild(buttonContainer);
-  });
-  block.appendChild(buttonWrapper);
-}
