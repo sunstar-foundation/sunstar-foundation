c1e89261bbe8c2fce1c8b397a92a370906c0814c
Dragoș-Cristian Bute <44490413+dnbute@users.noreply.github.com>
2024-01-10 16:29:15 +0200

    Refactored table for awards page (#87)
    * Refactored table to not require all config options
    
    * Style changes
    
    * css refactor to accomodate other tables
    
    * cleanup css to make inner-table the default
    
    ---------
    
    Co-authored-by: Dragos Cristian Bute <dbute@Dragoss-MacBook-Pro.local>

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,3 +1,5 @@
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
+
 function buildMutipleTables(block) {
   const mainContent = block.parentNode.parentNode;
   if (mainContent.querySelectorAll(':scope > .flat-table').length < 2) return;
@@ -33,39 +35,45 @@ function buildMutipleTables(block) {
 }
 
 export default async function decorate(block) {
+  const blockConfig = readBlockConfig(block);
+  const missingCfgs = [blockConfig.title, blockConfig.annotation].filter((x) => !x).length;
   const tableDiv = document.createElement('div');
   tableDiv.classList.add('table-item');
-  const tableTitle = document.createElement('h3');
-  tableTitle.classList.add('table-title');
   const tableAnnotation = document.createElement('p');
   tableAnnotation.classList.add('table-annotation');
-  [...block.children].forEach((child, i) => {
-    if (i === 0) {
-      tableTitle.innerHTML = [...child.children][1].innerHTML;
-    } else if (i === 1) {
-      tableAnnotation.innerHTML = [...child.children][1].innerHTML;
-    } else {
-      if (tableDiv.getAttribute('style') === null) {
-        const rowLength = ([...block.children].length - 2).toString();
-        const colLength = ([...child.children].length - 1).toString();
-        tableDiv.setAttribute('style', `--row:${rowLength};--col:${colLength}`);
-      }
-      [...child.children].forEach((childDiv, x) => {
-        if (x) {
-          if (i === 2) childDiv.classList.add('table-head');
-          tableDiv.append(childDiv);
-        }
-      });
+  const tableTitle = document.createElement('h3');
+  tableTitle.classList.add('table-title');
+
+  if (blockConfig.title) {
+    tableTitle.innerHTML = blockConfig.title;
+  }
+
+  if (blockConfig.annotation) {
+    tableAnnotation.innerHTML = blockConfig.annotation;
+  }
+
+  const tableChild = [...block.children].filter((x) => x.innerText.trim().toLowerCase().startsWith('table'))[0];
+  if (tableDiv.getAttribute('style') === null) {
+    const rowLength = ([...block.children].length - 2 + missingCfgs).toString();
+    const colLength = (tableChild.children.length - 1).toString();
+    tableDiv.setAttribute('style', `--row:${rowLength};--col:${colLength}`);
+  }
+
+  [...tableChild.children].forEach((childDiv, x) => {
+    if (x) {
+      childDiv.classList.add('table-head');
+      tableDiv.append(childDiv);
     }
   });
-  if (tableTitle.innerHTML === '') {
+
+  if (!blockConfig.title) {
     block.replaceChildren(tableDiv);
-    if (tableAnnotation.innerHTML !== '') block.append(tableAnnotation);
   } else {
     block.replaceChildren(tableTitle);
     block.append(tableDiv);
-    if (tableAnnotation.innerHTML !== '') block.append(tableAnnotation);
   }
+
+  if (blockConfig.annotation) block.append(tableAnnotation);
   if (block.classList.contains('flat')) block.parentElement.classList.add('flat-table');
   buildMutipleTables(block);
 }
