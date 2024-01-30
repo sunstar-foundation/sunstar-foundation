3e9915749183109d3c9900151bc8d119ba9bcd30
meejain <37831850+meejain@users.noreply.github.com>
2024-01-30 15:12:50 +0530

    Issue159 newsroom fix (#160)
    * Fixing the same logic for the latest newsroom article as we did for sunstar.com
    
    * Updating the logic for the miss - if currentLength + word.length + 1 > maxLength means croppedString will be null

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -864,6 +864,11 @@ export function cropString(inputString, maxLength) {
     return false;
   });
 
+  // If currentLength + word.length + 1 > maxLength means croppedString will be null hence
+  if (croppedString === '') {
+    croppedString = words[0].substring(0, maxLength);
+  }
+
   // Remove trailing space and add an ellipsis if needed
   croppedString = croppedString.trim();
   if (croppedString.length < inputString.length) {
