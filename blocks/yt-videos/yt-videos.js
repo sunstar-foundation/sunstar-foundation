a2f981a28b6137a0d64417935c7c0535def77e57
Meet Kumar Jain <meetkjain83@gmail.com>
2024-02-28 06:06:00 +0530

    Fixing the lint issues

--- blocks/yt-videos/yt-videos.js
+++ blocks/yt-videos/yt-videos.js
@@ -1,29 +1,26 @@
-import { handleModalClick, MODAL_FRAGMENTS_PATH_SEGMENT } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-    [...block.children].forEach((row) => {
-          // add video modal support if there is an anchor (to a video) and a picture
-          const anchor = row.querySelector('a');
-          const picture = row.querySelector('picture');
-          const anchorIsModal = anchor && anchor.classList.contains('video-link');
-  
-          if (picture && anchorIsModal) {
-            row.classList.add('video-modal');
-  
-            // add the picture inside the anchor tag and remove the text
-            anchor.textContent = '';
-            anchor.classList.add('video-modal');
-            anchor.appendChild(picture);
-  
-            // remove empty paragraphs
-            row.querySelectorAll('p').forEach((p) => {
-              if (!p.classList.contains('button-container')) { 
-                p.remove(); 
-            }
-            });
-  
-            picture.querySelector('img').classList.add('video-modal');
-          }
-    });
-  }
-  
+  [...block.children].forEach((row) => {
+    // add video modal support if there is an anchor (to a video) and a picture
+    const anchor = row.querySelector('a');
+    const picture = row.querySelector('picture');
+    const anchorIsModal = anchor && anchor.classList.contains('video-link');
+
+    if (picture && anchorIsModal) {
+      row.classList.add('video-modal');
+
+      // add the picture inside the anchor tag and remove the text
+      anchor.textContent = '';
+      anchor.classList.add('video-modal');
+      anchor.appendChild(picture);
+
+      // remove empty paragraphs
+      row.querySelectorAll('p').forEach((p) => {
+        if (!p.classList.contains('button-container')) {
+          p.remove();
+        }
+      });
+
+      picture.querySelector('img').classList.add('video-modal');
+    }
+  });
+}
