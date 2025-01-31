1a10a1f7674a1f7e608d14d01131ff880f80ba30
ehrro <dejan.eric@outlook.com>
2025-01-31 12:01:32 +0100

    Card 2 cols + several fix for TOC block

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,22 +1,22 @@
-import { getNamedValueFromTable } from '../../scripts/scripts.js';
+import { getNamedValueFromTable } from "../../scripts/scripts.js";
 
 function addEvent(menu, content) {
-  menu.addEventListener('click', () => {
-    content.classList.toggle('visible');
-    menu.classList.toggle('visible');
+  menu.addEventListener("click", () => {
+    content.classList.toggle("visible");
+    menu.classList.toggle("visible");
   });
 }
 
 function buildUl(ul, block) {
-  let textContent = '';
-  block.querySelectorAll('ul li').forEach((li) => {
-    const aLink = li.querySelectorAll('a')[0];
-    aLink.target = '_self';
-    if (li.querySelector('strong') !== null) {
+  let textContent = "";
+  block.querySelectorAll("ul li").forEach((li) => {
+    const aLink = li.querySelectorAll("a")[0];
+    aLink.target = "_self";
+    if (li.querySelector("strong") !== null) {
       textContent = aLink.textContent;
-      li.innerHTML = '';
+      li.innerHTML = "";
       li.append(aLink);
-      li.classList.add('active');
+      li.classList.add("active");
       ul.append(li);
     }
     ul.append(li);
@@ -25,71 +25,78 @@ function buildUl(ul, block) {
 }
 
 function decorateTOC(block) {
-  const menuTitle = getNamedValueFromTable(block, 'MobileMenuTitle');
-  const content = getNamedValueFromTable(block, 'Content');
-  const menuTitleTag = document.createElement('p');
-  menuTitleTag.classList.add('toc-menu-title');
+  const menuTitle = getNamedValueFromTable(block, "MobileMenuTitle");
+  const content = getNamedValueFromTable(block, "Content");
+  const menuTitleTag = document.createElement("p");
+  menuTitleTag.classList.add("toc-menu-title");
   menuTitleTag.textContent = menuTitle.textContent;
   addEvent(menuTitleTag, content);
-  const ul = content.querySelector('ul');
+  const ul = content.querySelector("ul");
   buildUl(ul, block);
-  content.classList.add('toc-content');
+  content.classList.add("toc-content");
   block.replaceChildren(menuTitleTag);
   block.append(content);
 }
 
 function buildTOCSide(block) {
   decorateTOC(block);
-  block.closest('.section').classList.add('right-toc');
-  const mainContent = document.createElement('div');
-  mainContent.classList.add('main-content');
-  const main = document.querySelector('main');
-  [...main.querySelectorAll('.section')].forEach((section) => {
-    if (!section.classList.contains('breadcrumb-container')) {
+  block.closest(".section").classList.add("right-toc");
+  const mainContent = document.createElement("div");
+  mainContent.classList.add("main-content");
+  const main = document.querySelector("main");
+  [...main.querySelectorAll(".section")].forEach((section) => {
+    if (!section.classList.contains("breadcrumb-container")) {
       mainContent.append(section);
     }
   });
-  const contentWrap = document.createElement('div');
-  contentWrap.classList.add('content-wrap');
-  const toc = mainContent.querySelector('.section.toc-container');
+  const contentWrap = document.createElement("div");
+  contentWrap.classList.add("content-wrap");
+  const toc = mainContent.querySelector(".section.toc-container");
   contentWrap.append(toc);
-  const contentInnercon = document.createElement('div');
-  contentInnercon.classList.add('content-innercon');
-  [...mainContent.querySelectorAll('.section.data-toc-section')].forEach((contentSection) => {
-    contentInnercon.append(contentSection);
-  });
+  const contentInnercon = document.createElement("div");
+  contentInnercon.classList.add("content-innercon");
+  [...mainContent.querySelectorAll(".section.data-toc-section")].forEach(
+    (contentSection) => {
+      contentInnercon.append(contentSection);
+    }
+  );
   contentWrap.append(contentInnercon);
-  mainContent.append(contentWrap);
+  const firstChild = mainContent.firstElementChild;
+  if (firstChild) {
+    mainContent.insertBefore(contentWrap, firstChild.nextSibling);
+  } else {
+    mainContent.append(contentWrap); // If no children, just append
+  }
   main.append(mainContent);
-  [...main.querySelectorAll('.section')].forEach((section) => {
-    [...section.querySelectorAll('.hidden.block')].forEach((hidden) => {
-      hidden.style.display = 'none';
+  [...main.querySelectorAll(".section")].forEach((section) => {
+    [...section.querySelectorAll(".hidden.block")].forEach((hidden) => {
+      hidden.style.display = "none";
     });
-    section.dataset.sectionStatus = 'loaded';
+    section.dataset.sectionStatus = "loaded";
     section.style.display = null;
   });
 }
 
 function buildTOCTop(ul, block) {
-  block.parentElement.classList.add('flat');
+  block.parentElement.classList.add("flat");
 
-  [...ul.querySelectorAll('li')].forEach((li) => {
+  [...ul.querySelectorAll("li")].forEach((li) => {
     const liVar = li.innerText.toLowerCase();
-    const aLink = document.createElement('a');
+    const aLink = document.createElement("a");
     aLink.href = `#${liVar}`;
     aLink.innerText = li.innerText;
-    li.innerText = '';
+    li.innerText = "";
     li.append(aLink);
   });
-  [...document.querySelectorAll('[data-toc-anchor]')].forEach((section) => {
+  [...document.querySelectorAll("[data-toc-anchor]")].forEach((section) => {
     const name = section.dataset.tocAnchor.toLowerCase().trim();
     section.id = name;
   });
 }
 
 export default async function decorate(block) {
-  if (block.classList.contains('flat')) {
-    const ul = block.querySelector('ul');
+  if (block.classList.contains("flat")) {
+    const ul = block.querySelector("ul");
     if (ul) {
       block.replaceChildren(ul);
     }
